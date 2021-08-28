79b0c3a7a936cfa5a2b58bc2efc28e11fb0eea0a
Monicamico <monica.amico@hotmail.it>
2021-08-28 18:42:24 +0200

    unica migrazione

--- test/Mayor.test.js
+++ test/Mayor.test.js
@@ -1,5 +1,5 @@
 const { assert } = require("chai")
-const _deploy_contracts = require("../migrations/2_deploy_contracts")
+const _deploy_contracts = require("../migrations/1_initial_migration")
 
 const Mayor = artifacts.require('Mayor.sol')
 
